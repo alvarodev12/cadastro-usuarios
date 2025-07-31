96430c722562e5b29c054484425cf2196511e1a4
alvarodev12 <alsouza23@outlook.com>
2025-07-31 16:14:27 -0300

    feat: add useEffect e useState

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,11 +1,31 @@
+import api from "../../services/api";
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button/styles";
 import TopBackgroud from "../../components/TopBackground";
 
 function ListUsers() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await api.get("/usuarios");
+      setUsers(data);
+    }
+    getUsers();
+  }, []);
+
   return (
     <div>
       <TopBackgroud />
       <h1>Listagem de Usuários</h1>
+
+      {users.map((user) => (
+        <div>
+          <p>{user.name}</p>
+          <p>{user.email}</p>
+          <p>{user.age}</p>
+        </div>
+      ))}
       <Button>Voltar</Button>
     </div>
   );
