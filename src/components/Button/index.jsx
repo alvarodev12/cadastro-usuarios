f9d12aa7333a1cb86b0ff11c15b69292d291cda8
alvarodev12 <alsouza23@outlook.com>
2025-07-31 15:05:44 -0300

    feat: validação prop-types

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
 import { Button } from "./styles";
 
 function DefaultButton({ children, ...props }) {
   return <Button {...props}>{children}</Button>;
 }
+
+DefaultButton.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default DefaultButton;
