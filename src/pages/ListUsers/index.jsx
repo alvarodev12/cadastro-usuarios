7a2db07d32d82b06e177eb5de3daee46a79dda39
alvarodev12 <alsouza23@outlook.com>
2025-07-31 20:14:12 -0300

    feat: add estilização e listagem de usuarios

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -2,9 +2,45 @@ import api from "../../services/api";
 import { useEffect, useState } from "react";
 import { Button } from "../../components/Button/styles";
 import TopBackgroud from "../../components/TopBackground";
+import Trash from "../../assets/trash.svg";
+
+import {
+  AvatarUser,
+  CardUsers,
+  Container,
+  ContainerUsers,
+  Title,
+  TrashIcon,
+} from "./styles";
 
 function ListUsers() {
-  const [users, setUsers] = useState([]);
+  // const [users] = useState([]);
+  let users = [
+    {
+      age: 39,
+      email: "maria@gmail.com",
+      id: "iuoho2873as0273027302uu209u32",
+      name: "Maria",
+    },
+    {
+      age: 37,
+      email: "luana@gmail.com",
+      id: "90821as9030820NH3291Casa3N28",
+      name: "Luana",
+    },
+    {
+      age: 27,
+      email: "jordan@gmail.com",
+      id: "908219030820NHasasas3291C3N28",
+      name: "Jordan",
+    },
+    {
+      age: 47,
+      email: "joao@gmail.com",
+      id: "908219030820NHasaassas3291C3N28",
+      name: "João",
+    },
+  ];
 
   useEffect(() => {
     async function getUsers() {
@@ -15,19 +51,28 @@ function ListUsers() {
   }, []);
 
   return (
-    <div>
+    <Container>
       <TopBackgroud />
-      <h1>Listagem de Usuários</h1>
+      <Title>Lista de Usuários</Title>
 
-      {users.map((user) => (
-        <div>
-          <p>{user.name}</p>
-          <p>{user.email}</p>
-          <p>{user.age}</p>
-        </div>
-      ))}
-      <Button>Voltar</Button>
-    </div>
+      <ContainerUsers>
+        {users.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUser
+              src={`https://avatar.iran.liara.run/public?username=${user.id}`}
+            />
+            <div>
+              <h3>{user.name}</h3>
+              <p>{user.age}</p>
+              <p>{user.email}</p>
+            </div>
+            <TrashIcon src={Trash} alt="icone-lixo" />
+          </CardUsers>
+        ))}
+      </ContainerUsers>
+
+      <Button type="button">Voltar</Button>
+    </Container>
   );
 }
 
